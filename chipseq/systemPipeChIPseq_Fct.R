fc81e944bd99bca4747bc7cb50afcdd02d951480
acarafat <ac.arafat@gmail.com>
2018-06-01 17:14:31 -0700

    final challenge

--- chipseq/systemPipeChIPseq_Fct.R
+++ chipseq/systemPipeChIPseq_Fct.R
@@ -1,4 +1,4 @@
-###############################                                                                                                                                               
+f##############################                                                                                                                                               
 ## PWM matching in sequences ##                                                                                                                                               
 ###############################                                                                                                                                               
                                                                                                                                                                               
@@ -21,5 +21,30 @@ ts <- motifMatch (as.list (pwm), as.list (pwm), top=11)
 #####################################
 ## Motif enrichment with PWMEnrich ##
 #####################################
-## see here: http://bioconductor.org/packages/release/bioc/vignettes/PWMEnrich/inst/doc/PWMEnrich.pdf 
+library(MotifDb)
+library(Biostrings) 
+library('PWMEnrich')
+
+peak_seq <- readDNAStringSet('results/SRR038848_1.fastq_trim.gz_C.bam_macs2_peaks.xls.fasta',format='fasta')
+athaliana_motifs <- as.list(query(MotifDb, 'athaliana'))
+
+# I need to check the pwm to pfm conversion
+# Is it mathmetically sound?
+pwm_to_pfm <- function(pwm){
+	pfm <- as.integer(as.matrix(pwm)*100000)
+	pfm <- matrix(pfm, nrow=4)
+	rownames(pfm) <- c('A', 'C', 'G', 'T')
+	colnames(pfm) <- 1:dim(pfm)[2]
+	return(pfm)
+}
+
+
+############################### 
+pfm_toy <- sapply(athaliana_motifs[1:5], pwm_to_pfm)
+#
+toy_bg <- makePWMLognBackground(peak_seq, pfm_toy)
+#
+res <- motifEnrichment(peak_seq[1], toy_bg)
+#
+
 
