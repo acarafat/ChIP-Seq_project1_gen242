e5ed886c91e221b0c804b34ff6ea44404376bc0f
acarafat <ac.arafat@gmail.com>
2018-06-03 23:52:46 -0700

    motif enrichment background calculation

--- chipseq/systemPipeChIPseq_Fct.R
+++ chipseq/systemPipeChIPseq_Fct.R
@@ -23,27 +23,34 @@ ts <- motifMatch (as.list (pwm), as.list (pwm), top=11)
 #####################################
 library(MotifDb)
 library(Biostrings) 
-library('PWMEnrich')
+library(PWMEnrich)
+library(GenomicFeatures)
 
 peak_seq <- readDNAStringSet('results/SRR038848_1.fastq_trim.gz_C.bam_macs2_peaks.xls.fasta',format='fasta')
 athaliana_motifs <- as.list(query(MotifDb, 'athaliana'))
 
-# I need to check the pwm to pfm conversion
-# Is it mathmetically sound?
-pwm_to_pfm <- function(pwm){
-	pfm <- as.integer(as.matrix(pwm)*100000)
+# PWMEnrich needs pfm as integer matrix 
+pfm_int_mat <- function(pfm){
+	pfm <- as.integer(as.matrix(pfm)*10000000)
 	pfm <- matrix(pfm, nrow=4)
 	rownames(pfm) <- c('A', 'C', 'G', 'T')
 	colnames(pfm) <- 1:dim(pfm)[2]
 	return(pfm)
 }
 
+# Convert MotifDb athaliana motifs into integer matrix 
+pfm <- sapply(athaliana_motifs, pfm_int_mat)
 
-############################### 
-pfm_toy <- sapply(athaliana_motifs[1:5], pwm_to_pfm)
-#
-toy_bg <- makePWMLognBackground(peak_seq, pfm_toy)
-#
+# Ned promoter sequence for calculating background frequencyi
+txdb <- loadDb("./data/TAIR10.sqlite")
+promoters <- promoters(genes(txdb), upstream = 1500, downstream = 500)
+genomeFa <- readDNAStringSet('data/tair10.fasta')
+promoter_seq<- getSeq(genomeFa, promoters[seqnames(promoters)=='Chr1' | seqnames(promoters)=='Chr2' | seqnames(promoters)=='Chr3' | seqnames(promoters)=='Chr4' | seqnames(promoters)=='Chr5' | seqnames(promoters)=='ChrC' | seqnames(promoters)=='ChrC'])
+
+# Motif enrichment needs LogN background
+PWM_bg <- makePWMLognBackground(clean(promoter_seq), pfm)
+
+# Motif enrichment
 res <- motifEnrichment(peak_seq[1], toy_bg)
 #
 
