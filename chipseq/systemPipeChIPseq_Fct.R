db60129800a013a1a93250b2d355984e4527e37d
acarafat <ac.arafat@gmail.com>
2018-06-10 12:32:51 -0700

    latest update on motif enrichment

--- chipseq/systemPipeChIPseq_Fct.R
+++ chipseq/systemPipeChIPseq_Fct.R
@@ -25,6 +25,8 @@ library(MotifDb)
 library(Biostrings) 
 library(PWMEnrich)
 library(GenomicFeatures)
+library(ShortRead)
+library(BSgenome)
 
 peak_seq <- readDNAStringSet('results/SRR038848_1.fastq_trim.gz_C.bam_macs2_peaks.xls.fasta',format='fasta')
 athaliana_motifs <- as.list(query(MotifDb, 'athaliana'))
@@ -41,17 +43,32 @@ pfm_int_mat <- function(pfm){
 # Convert MotifDb athaliana motifs into integer matrix 
 pfm <- sapply(athaliana_motifs, pfm_int_mat)
 
-# Ned promoter sequence for calculating background frequencyi
+# Need promoter sequence for calculating background frequency
 txdb <- loadDb("./data/TAIR10.sqlite")
-promoters <- promoters(genes(txdb), upstream = 1500, downstream = 500)
+promoters <- promoters(genes(txdb), upstream = 1000, downstream = 0)
+#promoters <- sample(promoters, 5000, replace=FALSE)
 genomeFa <- readDNAStringSet('data/tair10.fasta')
-promoter_seq<- getSeq(genomeFa, promoters[seqnames(promoters)=='Chr1' | seqnames(promoters)=='Chr2' | seqnames(promoters)=='Chr3' | seqnames(promoters)=='Chr4' | seqnames(promoters)=='Chr5' | seqnames(promoters)=='ChrC' | seqnames(promoters)=='ChrC'])
+#promoter_seq <- getSeq(genomeFa, promoters)
+promoter_seq<- getSeq(genomeFa, promoters[seqnames(promoters)=='Chr1' | 
+		      seqnames(promoters)=='Chr2' | seqnames(promoters)=='Chr3' | 
+		      seqnames(promoters)=='Chr4' | seqnames(promoters)=='Chr5'])
 
 # Motif enrichment needs LogN background
-PWM_bg <- makePWMLognBackground(clean(promoter_seq), pfm)
+sample_promoter_seqs <- sample(promoter_seq, 1000, replace=FALSE)
+PWMLogn.tair10.MotifDb.Athal<- makePWMLognBackground(clean(promoter_seq), pfm)
+save(PWM_bg, file='PWMLogn.tair10.MotifDb.Athal')
+load('Athal.TAIR10.PWM.bg')
 
-# Motif enrichment
-res <- motifEnrichment(peak_seq[1], toy_bg)
-#
+# Motif enrichment for single peak sequences
+res <- sapply(peak_seq, function(x) motifEnrichment(x, PWM_bg))
+report <- sequenceReport(res[[1]], 1)
+plot(report[report$p.value < 0.05], fontsize=7, id.fontsize=6)
 
+# Motif enrichment in multiple sequences
+res <- motifEnrichment(peak_seq, PWM_bg)
+report <- groupReport(res)
+report
 
+png(filename='group_enriched_motif.png')
+plot(report[1:10], fontsize=7, id.fontsize=5)
+dev.off()
